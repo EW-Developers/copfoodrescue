70d131cf834dd6c174e94fe069a870e088e3d678
AAAA477 <134492910+AAAA477@users.noreply.github.com>
2026-05-17 22:22:08 -0600

    Fix GitHub Pages static entry

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/copfoodrescue/',
+  build: {
+    rollupOptions: {
+      input: 'src/main.jsx',
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/index.js',
+      },
+    },
+  },
   plugins: [react()],
 })
